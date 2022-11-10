f0ee388ee6ab11ff890fe679a21730c1cfc164c6
Vineet Parmar <vineetjparmar@gmail.com>
2022-11-11 00:52:11 +0530

    Add files via upload

---
+++ DBMS v3.1/DBMS/website/views.py
@@ -0,0 +1,33 @@
+#from unicodedata import category
+from flask import Blueprint,render_template,flash,request,jsonify
+from flask_login import login_required,current_user
+from .models import Note
+from . import db
+import json
+views=Blueprint('views',__name__)
+
+
+@views.route('/teacher-profile')
+@login_required
+def teacher_profile():
+    return render_template("teacher_profile.html",user=current_user)
+
+@views.route('/teacher-courses')
+@login_required
+def teacher_courses():
+    return render_template("teacher_courses.html",user=current_user)
+
+@views.route('/student-profile')
+@login_required
+def student_profile():
+    return render_template("student_profile.html",user=current_user)
+
+@views.route('/student-courses')
+@login_required
+def student_courses():
+    return render_template("student_courses.html",user=current_user)
+
+@views.route('/student-grades')
+@login_required
+def student_grades():
+    return render_template("student_grades.html",user=current_user)
